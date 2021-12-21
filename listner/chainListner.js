5a1c8c1d7a88a273154cd8ae9eb6e1aed54cb5dd
Raja Anche <anche.raja@gmail.com>
2021-12-21 11:06:45 -0500

    Updated Average transactions

--- listner/chainListner.js
+++ listner/chainListner.js
@@ -10,27 +10,34 @@ async function main() {
     provider = new WsProvider(process.env.ws_node);
     const api = await ApiPromise.create({ provider });
     let count = 0;
+    let blockCounts = 0;
+    let totalTransactions = 0;
     // Subscribe to the new headers on-chain. The callback is fired when new headers
     // are found, the call itself returns a promise with a subscription that can be
     // used to unsubscribe from the newHead subscription
 
     const unsubscribe = await api.rpc.chain.subscribeFinalizedHeads(async (header) => {
         logger.info(`Chain is at finalized block: #${header.number}`);
-        const blockHash = await api.rpc.chain.getBlockHash(header.number);
 
+        const blockHash = await api.rpc.chain.getBlockHash(header.number);
         const signedBlock = await api.rpc.chain.getBlock(blockHash);
         let txnCounter = 0;
+
         signedBlock.block.extrinsics.forEach((ex, index) => {
             let event = ex.toHuman();
             //logger.info(JSON.stringify(event));
             if (event.isSigned) {
-                logger.info(`${JSON.stringify(event.signer.Id)} ==> ${JSON.stringify(event.method.args.dest.Id)}`);
+                //logger.info(`${JSON.stringify(event.signer.Id)} ==> ${JSON.stringify(event.method.args.dest.Id)}`);
                 txnCounter++;
+                totalTransactions++
             }
             //if (event.method.method == 'transfer') txnCounter++
         });
+        if (txnCounter > 0) blockCounts++;
         logger.info(`${txnCounter} transactions finalized in block: #${header.number}`);
+        logger.info(`Total transactions #${totalTransactions} in finalized blocks: #${blockCounts}, Average transactions: ${totalTransactions / blockCounts}`);
         if (++count === 256) {
+            logger.info("Counter > 256.. Exiting...");
             unsubscribe();
             process.exit(0);
         }
